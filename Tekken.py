4282dde0bf35900dac03bce9440dd4879a481025
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-03-16 14:52:26 +0530

    codechef

---
+++ Tekken.py
@@ -0,0 +1,10 @@
+for _ in range (int(input())):
+    a,b,c = map(int,input().split())
+    min3=min(b,c)
+    p = b - min3
+    q = c - min3
+    sum = p+q
+    if a!=0 and a>sum:
+        print("Yes")
+    else :
+        print('No')
